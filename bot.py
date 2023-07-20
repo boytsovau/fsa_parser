49d63fb16dea0ff00650d5cf7f94b8bedd3535e3
boytsovau <alexbonch@gmail.com>
2023-07-20 11:28:02 +0300

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,9 @@ async def start(message: types.Message):
 @dp.message_handler()
 async def get_info(message: types.Message):
     await message.answer("Нужно подождать.....")
+    user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+    with open("users.log", "w") as file:
+        file.writelines(user_status.user.username)
 
     result = get_declaration(message.text)
 
