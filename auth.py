17d597b1f5f6dc3c0b7b3516aea3eac52803b34e
boytsovau <alexbonch@gmail.com>
2024-02-06 16:18:24 +0300

    modified:   auth.py 	modified:   bot.py 	modified:   main.py 	modified:   multi.py

--- auth.py
+++ auth.py
@@ -1,32 +1,55 @@
 import requests
 from fake_useragent import UserAgent
 from proxy_data import proxies
-
-ua = UserAgent()
-
-
-headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Authorization': 'Bearer null',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/json',
-    'Origin': 'https://pub.fsa.gov.ru',
-    'Referer': 'https://pub.fsa.gov.ru/rds/declaration',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': f'{ua.random}',
-}
-
-token = {'token': 'Bearer null'}
-
-json_data = {
-    'username': 'anonymous',
-    'password': 'hrgesf7HDR67Bd',
-}
+from dotenv import load_dotenv
 
 
+load_dotenv()
+
+class FsaAuth:
+    def __init__(self):
+        self.ua = UserAgent()
+        self.headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Authorization': 'Bearer null',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Origin': 'https://pub.fsa.gov.ru',
+            'Referer': 'https://pub.fsa.gov.ru/rds/declaration',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': f'{self.ua.random}',
+        }
+        self.token = {'token': 'Bearer null'}
+        self.json_data = {
+            'username': 'anonymous',
+            'password': 'hrgesf7HDR67Bd',
+        }
+
+    def get_auth(self, url='https://pub.fsa.gov.ru/login'):
+        response = requests.post(url, headers=self.headers, json=self.json_data, verify=False)
+        data = dict(response.headers)
+        self.token['token'] = data.get('Authorization')
+
+    def validation_token(self):
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'ru-RU,ru;q=0.9',
+            'Authorization': self.token.get('token'),
+            'Connection': 'keep-alive',
+            'Referer': 'https://pub.fsa.gov.ru/rds/declaration',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': f'{self.ua.random}',
+        }
+        response = requests.get('https://pub.fsa.gov.ru/token/is/actual/', headers=headers, verify=False)
+        valid = response.text
+        return valid
+
+<<<<<<< HEAD
 def get_auth(url='https://pub.fsa.gov.ru/login'):
     response = requests.post(
         url,
@@ -64,7 +87,15 @@ def get_token(token=token):
         return token
     else:
         get_auth()
+=======
+    def get_token(self):
+        if self.validation_token() == 'true':
+            return self.token
+        else:
+            self.get_auth()
+>>>>>>> 0a3e18c (	modified:   auth.py)
 
 
 if __name__ == "__main__":
-    get_token(token)
+    fsa_client = FsaAuth()
+    fsa_client.get_token()
